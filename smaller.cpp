0e7102f954813fa1761a270e31caea4c3372d2ea
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-08-31 15:20:53 -0400

    Added smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,9 @@
+/*
+Author: Natalie Gallo
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab1A (smaller.cpp)
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
