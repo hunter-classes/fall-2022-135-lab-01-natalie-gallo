b4fba2530cf64c76aec875864651aba69f0da7c6
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-08-31 19:05:24 -0400

    completed smaller.cpp

--- smaller.cpp
+++ smaller.cpp
@@ -4,6 +4,26 @@ Course: CSCI-135
 Instructor: Michael Zamansky
 Assignment: Lab1A (smaller.cpp)
 
-Here, briefly, at least in one or a few sentences
-describe what the program does.
+Asks user for two numbers and prints smaller of the two.
 */
+
+#include <iostream>
+
+int main()
+{
+  int num_1;
+  int num_2;
+
+  std::cout << "Enter the first number: \n";
+  std::cin >> num_1;
+  std::cout << "Enter the second number: \n";
+  std::cin >> num_2;
+
+  if (num_1 < num_2) {
+    std:: cout << "The smaller of the two is " << num_1 << std::endl;
+  } else {
+    std::cout << "The smaller of the two is " << num_2 << std::endl;
+  }
+  return 0;
+}
+  
